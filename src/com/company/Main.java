2845f1529ce1a9e8d99010a1f77ba3e89b0cc29c
Denis <deniska2019@yandex.ru>
2022-03-29 21:14:46 +0300

    nome2.1-5ds

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -53,6 +53,39 @@ public class Main {
       double averagetwo = weightlosGr / averageone;
       System.out.println("Среднее количество дней, необходимое для похудения " + averagetwo );
 
+      int masha = 67760;
+      int denis = 83690;
+      int kristina = 76230;
+      double sal = 0.1;
+      int year = 12;
+
+      double raisingm = masha * sal;
+      double mashaernone = masha + raisingm;
+      int mashaertwo = masha * year;
+      double zpm = mashaernone * year;
+      double ma = zpm - mashaertwo;
+      System.out.println("Маша теперь получает в месяц " + mashaernone + " рублей");
+      System.out.println("Годовой доход Маши вырос на " + ma + " рублей");
+
+      double raisingd = denis * sal;
+      double denisernone = denis + raisingd;
+      int denisertwo = denis * year;
+      double zpd = denisernone * year;
+      double da = zpd - denisertwo;
+      System.out.println("Денис теперь получает в месяц " + denisernone + " рублей");
+      System.out.println("Годовой доход Дениса вырос на " + da + " рублей");
+
+
+
+      double raisingk = kristina * sal;
+      double kristinaernone = kristina + raisingk;
+      int kristinaertwo = kristina * year;
+      double zpk = kristinaernone * year;
+      double ka = zpk - kristinaertwo;
+      System.out.println("Кристина теперь получает в месяц " + kristinaernone + " рублей");
+      System.out.println("Годовой доход Кристины вырос на " + ka + " рублей");
+
+
 
 
 
