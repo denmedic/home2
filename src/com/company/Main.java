9655024001298b7ba26876ac8b3d1384180a9121
Denis <deniska2019@yandex.ru>
2022-03-29 17:32:48 +0300

    nome2.1ds

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+      int a = 10;
+      byte b = 1;
+      short c = 1000;
+      long d = 10000;
+      float e = 1.75f;
+      double f = 2.6;
     }
 }
